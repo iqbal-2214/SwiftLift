344c9e6ce96a8722a87853f7709562ac18700eb5
iqbal <iqbalrimtian2214@gmail.com>
2025-08-05 05:06:31 +0530

    Feat: Use environment variables for Supabase keys

--- src/superBaseClient.js
+++ src/superBaseClient.js
@@ -1,9 +1,6 @@
-// /src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// --- IMPORTANT: REPLACE with your own Supabase project details ---
-const supabaseUrl = 'https://ulxyefrtgogcqekjgang.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseHllZnJ0Z29nY3Fla2pnYW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzMjgwMTIsImV4cCI6MjA2OTkwNDAxMn0.HzaofgNOpgyukqbrxel5z3WHKf3fAlJ7eXnlFpH2ZRo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Create and export the Supabase client. This single object is our gateway to the backend.
 export const supabase = createClient(supabaseUrl, supabaseKey);
